00922f88689359eb9fb4069822107a15c2f6849c
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-19 16:53:24 +0100

    basic layout about the finder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
     import "../app.css"
 </script>
 
-<main>
-    <slot></slot>
+<main class="w-screen h-screen grid grid-rows-[58px_1fr_44px] ">
+    <section class="flex border-b-[1px] border-white/[.1] justify-start items-center p-4 space-x-2">
+        <h3 class="text-white">Header</h3>
+    </section>
+    <section class="">
+        <slot></slot>
+    </section>
+    <section class="bg-black/[.35] flex justify-start items-center p-4 space-x-2">
+        <h3 class="text-white">Footer</h3>
+    </section>
 </main>
