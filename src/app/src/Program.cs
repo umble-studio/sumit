f58f81a2e7676c2c9d53ef8d03b66b49d0df548d
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 20:43:45 +0100

    Register iconify

--- src/app/src/Program.cs
+++ src/app/src/Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using app;
 using app.Services.Extension;
+using Iconify.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddIconify();
 builder.Services.AddSingleton<ExtensionRegistry>();
 builder.Services.AddSingleton<ExtensionManager>();
 
