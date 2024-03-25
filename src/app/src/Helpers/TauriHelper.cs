a2b847ccbc85d7efce86c95cac761780d7b818a7
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 10:58:08 +0100

    Add readFile fs function (need to be refactored later)

--- src/app/src/Helpers/TauriHelper.cs
+++ src/app/src/Helpers/TauriHelper.cs
@@ -6,6 +6,16 @@ public static class TauriExtensions
     {
         return await js.InvokeAsync<bool>("fs_exists", path);
     }
+
+    public async static Task<string> FsReadTextFile(this IJSRuntime js, string path)
+    {
+        return await js.InvokeAsync<string>("fs_readTextFile", path);
+    }
+
+    public async static Task<byte[]> FsReadFile(this IJSRuntime js, string path)
+    {
+        return await js.InvokeAsync<byte[]>("fs_readFile", path);
+    }
     
     public async static Task<string> GetTauriVersion(this IJSRuntime js)
     {
--- src/app/src/wwwroot/tauri.js
+++ src/app/src/wwwroot/tauri.js
@@ -4,10 +4,24 @@ const http = window.__TAURI__.http;
 const notification = window.__TAURI__.notification;
 const fs = window.__TAURI__.fs;
 
+// import { readFileText } from '@tauri-apps/plugin-fs';
+
 window.fs_exists = async (path) => {
 	const exists = await fs.exists(path, { baseDir: 6 });
 	console.log("exists: " + exists);
 	return exists;
+}
+
+window.fs_readTextFile = async (path) => {
+	const text = await fs.readTextFile(path, { baseDir: 6 });
+	console.log("text: " + text);
+	return text;
+}
+
+window.fs_readFile = async (path) => {
+	const buffer = await fs.readFile(path, { baseDir: 6 });
+	console.log("buffer: " + buffer);
+	return buffer;
 }
 
 window.getTauriVersion = async () => {
