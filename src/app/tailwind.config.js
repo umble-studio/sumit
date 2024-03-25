6dc0e20b023e8db2b786379c6b9bfeadfd115482
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 20:44:18 +0100

    Add path in tailwind config to watch iconify lib

--- src/app/tailwind.config.js
+++ src/app/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,cshtml,js,ts,razor}'],
+  content: [
+    // './src/**/*.{html,cshtml,js,ts,razor}',
+    '../**/*.razor',
+    '../../../Iconify/**/*.razor',
+  ],
   theme: {
     extend: {}
   },
