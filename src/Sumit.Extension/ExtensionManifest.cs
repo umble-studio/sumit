597cf35f93993a3d1f3ffe2c0b4b904df815cbb8
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 11:42:11 +0100

    Extension manifest to describe extension informations

---
+++ src/Sumit.Extension/ExtensionManifest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sumit.Extension;
+
+public record ExtensionManifest
+{
+    [JsonPropertyName("name")] public string Name { get; init; }
+    [JsonPropertyName("description")] public string Description { get; init; }
+    [JsonPropertyName("version")] public string Version { get; init; }
+    [JsonPropertyName("author")] public string Author { get; init; }
+    [JsonPropertyName("license")] public string License { get; init; }
+}
