c680475282c4290ca1fbfc37245154c35f0144e7
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 17:12:24 +0100

    Update footer + header and add start to create a text input component

---
+++ src/app/src/Components/TextInput.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace app.Components;
+
+public partial class TextInput : ComponentBase
+{
+    [Parameter] public string Placeholder { get; set; }
+    [Parameter] public string Value { get; set; }
+}
