7c00e3746af0357196770500b81843ecbf3c9f14
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 14:35:25 +0100

    Fix wrong property case

--- src/Sumit.Extension/ExtensionManifest.cs
+++ src/Sumit.Extension/ExtensionManifest.cs
@@ -9,11 +9,14 @@ public record ExtensionManifest
     [JsonPropertyName("description")] public string Description { get; init; } = null!;
     [JsonPropertyName("version")] public string Version { get; init; } = null!;
     [JsonPropertyName("author")] public string Author { get; init; } = null!;
-    [JsonPropertyName("license"), AllowedValues("MIT")] public string License { get; init; } = null!;
+
+    [JsonPropertyName("license"), AllowedValues("MIT")]
+    public string License { get; init; } = null!;
+
     [JsonPropertyName("client")] public ClientData Client { get; init; } = null!;
 
     public record ClientData
     {
-        public string EntryPoint { get; init; }
+        [JsonPropertyName("entrypoint")] public string EntryPoint { get; init; }
     }
 }
