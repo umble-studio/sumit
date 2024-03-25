9450da17428f401dd5b17a83824fcbd57676f1bc
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 13:56:15 +0100

    Extension manifest now contains a client entrypoint

--- src/Sumit.Extension/ExtensionManifest.cs
+++ src/Sumit.Extension/ExtensionManifest.cs
@@ -1,12 +1,19 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Sumit.Extension;
 
 public record ExtensionManifest
 {
-    [JsonPropertyName("name")] public string Name { get; init; }
-    [JsonPropertyName("description")] public string Description { get; init; }
-    [JsonPropertyName("version")] public string Version { get; init; }
-    [JsonPropertyName("author")] public string Author { get; init; }
-    [JsonPropertyName("license")] public string License { get; init; }
+    [JsonPropertyName("name")] public string Name { get; init; } = null!;
+    [JsonPropertyName("description")] public string Description { get; init; } = null!;
+    [JsonPropertyName("version")] public string Version { get; init; } = null!;
+    [JsonPropertyName("author")] public string Author { get; init; } = null!;
+    [JsonPropertyName("license"), AllowedValues("MIT")] public string License { get; init; } = null!;
+    [JsonPropertyName("client")] public ClientData Client { get; init; } = null!;
+
+    public record ClientData
+    {
+        public string EntryPoint { get; init; }
+    }
 }
