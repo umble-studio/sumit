045b53e7fde1d218f76fcc4ef1a86ea83a98a98a
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-26 10:32:23 +0100

    The icon of a button can now be set

--- src/app/src/Components/ButtonIcon.razor.cs
+++ src/app/src/Components/ButtonIcon.razor.cs
@@ -4,4 +4,6 @@ namespace app.Components;
 
 public partial class ButtonIcon : ComponentBase
 {
+    [Parameter] public Dictionary<string, object> Attributes { get; set; } = null!;
+    [Parameter] public string Icon { get; set; } = null!;
 }
