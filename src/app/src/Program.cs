ba333998c8d4baaf4bb3cf81ac07bf4692fb5cdf
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-27 14:51:09 +0100

    Register file watcher and hot reload services

--- src/app/src/Program.cs
+++ src/app/src/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using app;
 using app.Services.Extension;
 using Iconify.Extensions;
+using app.Services.FileWatcher;
+using app.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,5 +14,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddIconify();
 builder.Services.AddSingleton<ExtensionRegistry>();
 builder.Services.AddSingleton<ExtensionManager>();
+builder.Services.AddScoped<FileWatcher>();
+builder.Services.AddScoped<Hotreload>();
 
 await builder.Build().RunAsync();
