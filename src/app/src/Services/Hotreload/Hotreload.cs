f6a423e45bed6c6a09a8e586ae3039f605322bfe
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-27 15:36:13 +0100

    Inject extension manager in hotreload service

--- src/app/src/Services/Hotreload/Hotreload.cs
+++ src/app/src/Services/Hotreload/Hotreload.cs
@@ -1,12 +1,17 @@
+using app.Services.Extension;
+
 namespace app.Services.HotReload;
 
 public sealed partial class HotReload
 {
     private readonly FileWatcher.FileWatcher _fileWatcher;
+    private readonly ExtensionManager _extensionManager;
 
-    public HotReload(FileWatcher.FileWatcher fileWatcher)
+    public HotReload(FileWatcher.FileWatcher fileWatcher, ExtensionManager extensionManager)
     {
         _fileWatcher = fileWatcher;
+        _extensionManager = extensionManager;
+        
         _fileWatcher.Changed += OnFileChanged;
     }
 
