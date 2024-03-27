dfb6db104a3d8348772e89e12e74d61cfd7c6aa8
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-27 14:50:53 +0100

    Add hot reload class, this is not working yet

---
+++ src/app/src/Services/Hotreload.cs
@@ -0,0 +1,24 @@
+using app.Services.FileWatcher;
+
+namespace app.Services;
+
+public sealed class Hotreload
+{
+    private readonly FileWatcher.FileWatcher _fileWatcher;
+
+    public Hotreload(FileWatcher.FileWatcher fileWatcher)
+    {
+        _fileWatcher = fileWatcher;
+        _fileWatcher.Changed += OnFileChanged;
+    }
+
+    public ValueTask Initialize()
+    {
+        return _fileWatcher.Initialize();
+    }
+
+    private void OnFileChanged(ChangedPayload payload)
+    {
+        
+    }
+}
