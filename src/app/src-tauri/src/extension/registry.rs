882f7e31f709720e20a2249811041b8bb26e6537
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-28 10:57:34 +0100

    Add load_manifests function to fetch all manifest.json files in the extension dir

--- src/app/src-tauri/src/extension/registry.rs
+++ src/app/src-tauri/src/extension/registry.rs
@@ -1,4 +1,8 @@
-use super::manifest::ExtensionManifest;
+use std::path::Path;
+use dirs::document_dir;
+use glob::glob;
+
+use super::{constant::EXTENSION_DIRECTORY, manifest::ExtensionManifest};
 
 #[derive(Debug, Default)]
 pub struct ExtensionRegistry {
@@ -22,6 +26,44 @@ impl ExtensionRegistry {
 
     pub fn get_manifest(&self, name: &str) -> Option<ExtensionManifest> {
         self.manifests.iter().find(|m| m.name == name).cloned()
+    }
+
+    pub fn load_manifests(&mut self) -> Vec<String> {
+        self.manifests.clear();
+
+        let document_dir = document_dir().unwrap();
+        let extension_dir = Path::join(&document_dir, EXTENSION_DIRECTORY);
+        let pattern = &format!("{}/**/*.json", extension_dir.to_str().unwrap().to_string());
+
+        println!("Looking for extension manifests in: {}", pattern);
+
+        let mut paths: Vec<String> = Vec::new();
+
+        for entry in glob(pattern).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path) => {
+                    println!("Found: {}", path.display());
+
+                    if let Some(prefix) = path.file_stem() {
+                        if prefix == "manifest" {
+                            println!("Loading manifest: {}", prefix.to_str().unwrap().to_string());
+
+                            let path = path.to_str().unwrap().to_string();
+                            paths.push(path.clone());
+
+                            if let Some(manifest) = self.get_manifest(path.as_str()) {
+                                self.manifests.push(manifest);
+                            }
+                        }   
+                    }
+                }
+                Err(err) => {
+                    println!("Error: {}", err);
+                }
+            }
+        }
+
+        paths
     }
 
     pub fn get_manifests(&self) -> Vec<ExtensionManifest> {
